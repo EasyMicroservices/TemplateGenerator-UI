f240946e5a29f3f970a366f984d2e33f09ec2528
Ali Yousefi <ali.visual.studio@gmail.com>
2024-01-06 12:09:06 +0330

    Add support for checkbox, checklist, datetime, label, multipleselect, timeonly

---
+++ src/CSharp/Cores/EasyMicroservices.UI.TemplateGenerator.ViewModels/ViewModels/Generators/Components/MultipleSelectFormItemViewModel.cs
@@ -0,0 +1,12 @@
+﻿using TemplateGenerators.GeneratedServices;
+
+namespace EasyMicroservices.UI.TemplateGenerator.ViewModels.Generators.Components;
+public class MultipleSelectFormItemViewModel : BaseGeneratorViewModel
+{
+    public MultipleSelectFormItemViewModel()
+    {
+
+    }
+
+    public IEnumerable<FormItemContract> SelectedValues { get; set; } = new HashSet<FormItemContract>();
+}
